83dd7e94375f2da9da9e1c51ad8e2423e6e26e3c
ZaydAbdullayev <zeyyadabuzayd@gmail.com>
2023-06-24 14:24:00 +0500

    new section

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -10,7 +10,6 @@ import axios from "axios";
 import { useDispatch, useSelector } from "react-redux";
 import { acLoading } from "../../redux/loading";
 import Skeleton from "react-loading-skeleton";
-import { Icon } from "../../components/icon/icon";
 
 const base_url = process.env.REACT_APP_BASE_URL;
 export const Home = () => {
@@ -94,8 +93,6 @@ export const Home = () => {
                 );
               })}
         </div>
-
-        <Icon />
 
         {/* ============ Conatct us section ========== */}
         <Contact />
